0ab62386f25f609949e090105965b551f6545fd0
Ubuntu <ubuntu@ip-172-31-11-255.eu-west-1.compute.internal>
2021-12-17 10:24:11 +0000

    instalador casi acabado

--- notas/dia5.md
+++ notas/dia5.md
@@ -16,3 +16,34 @@
            value: 1.14.0
 
 ]
+
+combine()
+currentVersions.VERSIONS | dict2items
+
+git: 2.34.1
+curl: 7.58.0
+nginx: 1.14.0
+
+  VVVVVV
+
+- key: curl
+value: 7.58.0 
+- key: git
+value: 2.34.1
+- key: nginx
+value: 1.14.0
+
+
+- 1
+- 2
+
+- a 
+- b 
+
+ZIP:
+
+- - 1
+  - a
+- - 2
+  - b
+
--- roles/instalador/templates/currentVersions.sh
+++ roles/instalador/templates/currentVersions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo [VERSIONS]
 {% for herramienta in versionesInstaladas.results %}
-echo {{ herramienta.item.key }}=$({{ herramienta.item.value.version.command | default (herramienta.item.key + " --version") }} | grep -Eo '[0-9]+[.][0-9]+([.][0-9]+)' | head -1 )
+echo {{ herramienta.item.key }}=$({{ herramienta.item.value.version.command | default (herramienta.item.key + " --version") }} | grep -Eo '[0-9]+[.][0-9]+([.][0-9a-z]+)' | head -1 )
 {% endfor %}
